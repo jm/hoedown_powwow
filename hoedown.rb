a6113c3a547e704d45e50841520c4719875805af
Jeremy McAnally <jeremymcanally@gmail.com>
2008-08-08 22:02:38 -0500

    Uh, for real this time.

--- hoedown.rb
+++ hoedown.rb
@@ -68,6 +68,16 @@ class Tweeter
   def friends_element
     document.at("div#friends")
   end
+  
+  def friend_elements
+    friends_element.search("span.vcard")
+  end
+  
+  def friends
+    friend_elements.map do |elem|
+      Friend.parse(elem)
+    end
+  end
 end
 
 class TweeterTest < Test::Unit::TestCase
@@ -86,30 +96,54 @@ class TweeterTest < Test::Unit::TestCase
   end
   
   # maybe do a test to grab a single friend first. Then we can work out the collection is grabbed.
-  #def test_should_have_a_friend
-  #  tweeter = Tweeter.new("hoedown")
-  #  assert_instance_of Friend, tweeter.friends.first
-  #end
+  def test_should_have_an_array_of_friend_elements
+    tweeter = Tweeter.new("hoedown")
+    elements = tweeter.friend_elements
+    assert_instance_of Hpricot::Elements, elements
+    assert elements.size > 0
+  end
   
-  # tests to check validity of friends div?
-  # we need to get people to input their logins :)
-  # then we can use the test to confirm they have
+  def test_should_have_an_array_of_friends
+    tweeter = Tweeter.new("hoedown")
+    friends = tweeter.friends
+    assert_instance_of Array, friends
+  end
+  
+  def test_should_have_an_array_of_friends_that_contains_a_friend
+    tweeter = Tweeter.new("hoedown")
+    assert_instance_of Friend, tweeter.friends.first
+  end
+
 end
 
 class Friend
-
+  attr_accessor :name
+  
+  def initialize(name)
+    @name = name
+  end
+  
+  def self.parse(elem)
+    name = elem.at("a").attributes["title"]
+    new(name)
+  end
 end
 
 class FriendTest < Test::Unit::TestCase
-  def test_should_be_able_to_parse_the_html_for_a_single_friend
-    xml = <<-XML
+  TEST_HTML = <<-XML
 <span class="vcard">
-    <a title="Someone's name" rel="contact" class="url" href="http://twitter.com/someone"><img width="24" height="24" src="http://s3.amazonaws.com/twitter_production/profile_images/zzzzzz/me_mini.jpg" id="profile-image" class="photo fn" alt="Alt"/></a>
+    <a title="Someone" rel="contact" class="url" href="http://twitter.com/someone"><img width="24" height="24" src="http://s3.amazonaws.com/twitter_production/profile_images/zzzzzz/me_mini.jpg" id="profile-image" class="photo fn" alt="Alt"/></a>
 </span>
-    XML
-    friend = Friend.parse(xml)
+  XML
+    
+  def elem
+    Hpricot(TEST_HTML).at("/")
+  end
+
+  def test_should_be_able_to_parse_the_html_for_a_single_friend
+    friend = Friend.parse(elem)
     assert_instance_of Friend, friend
-    assert_equal "
+    assert_equal "Someone", friend.name
   end
 end
 
@@ -152,3 +186,33 @@ def zomg_this_is_a_crazie_method_that_will_pretty_much_screw_you_like_woah!
     end
   end
 end
+
+FULL_HTML = <<-EOT
+<div class="section">
+    <div class="section-header">
+      <h1>Following</h1>
+    </div><!-- /section-header -->
+
+    
+      <div id="friends">
+        
+  <span class="vcard">
+    <a title="Michael Ivey" rel="contact" class="url" href="http://twitter.com/ivey"><img width="24" height="24" src="http://s3.amazonaws.com/twitter_production/profile_images/54932025/newprofile_mini.jpg" id="profile-image" class="photo fn" alt="Michael Ivey"/></a>
+  </span>
+
+
+  <span class="vcard">
+    <a title="Tommy Morgan" rel="contact" class="url" href="http://twitter.com/duwanis"><img width="24" height="24" src="http://s3.amazonaws.com/twitter_production/profile_images/57746410/fryvatar_mini.jpg" id="profile-image" class="photo fn" alt="Tommy Morgan"/></a>
+  </span>
+
+
+  <span class="vcard">
+    <a title="David A. Black" rel="contact" class="url" href="http://twitter.com/david_a_black"><img width="24" height="24" src="http://s3.amazonaws.com/twitter_production/profile_images/56329703/me_mini.jpg" id="profile-image" class="photo fn" alt="David A. Black"/></a>
+  </span>
+
+
+      </div>
+          
+  </div>
+EOT
+
